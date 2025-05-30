a51d3a9ae164bf235535b424a45be45518cb9db5
AsulaML <michaellambert649@gmail.com>
2025-05-30 21:39:42 +0200

    Add files via upload

---
+++ TPS55288.c
@@ -0,0 +1,184 @@
+#include "main.h"
+#include "devices.h"
+#include "TPS55288.h"
+#include "I2C.h"
+#include "board.h"
+#include "mcc_generated_files/adc1.h"
+
+struct TPS BuckBoost;
+
+/**
+ * \fn void TPS55288_SetVout(uint8_t SlaveAdr, float vout)
+ * \brief fonction qui permet de convertir la tension en val registre
+ * et de l'envoyer au composant pour changer sa consigne de tension
+ * \return 0
+ */
+void TPS55288_SetVout(uint8_t SlaveAdr, float vout)
+{
+    uint16_t RegVref = 0;
+    
+    RegVref = (vout-0.6)/0.015;
+    
+    TPS55288_Write_VREF(SlaveAdr, RegVref);
+}
+
+
+/**
+ * \fn void TPS55288_Init(uint8_t SlaveAdr)
+ * \brief fonction qui permet d'initialiser le composant
+ * \return 0
+ */
+void TPS55288_Init(uint8_t SlaveAdr)
+{   
+    // clear bot OCP_MASK and SCP MASK
+    TPS55288_W_REG(SlaveAdr, REG_CDC, BuckBoost.CDC & 0x3F); //BFs
+    BuckBoost.CDC = TPS55288_R_REG(SlaveAdr, REG_MODE);
+    
+    // Config de la limite de courant
+    BuckBoost.I_OUT_LIMIT = TPS_IOUT_LIMIT_1_5A;
+    BuckBoost.I_OUT_LIMIT |= TPS_IOUT_LIMIT_ENABLE; 
+    TPS55288_W_REG(SlaveAdr, REG_IOUT_LIMIT, BuckBoost.I_OUT_LIMIT);
+    BuckBoost.I_OUT_LIMIT = TPS55288_R_REG(SlaveAdr, REG_IOUT_LIMIT);
+    
+    TPS55288_W_REG(SlaveAdr, REG_MODE, TPS_OE_ON);
+    BuckBoost.MODE = TPS55288_R_REG(SlaveAdr, REG_MODE);
+    
+    TPS55288_W_REG(SlaveAdr, REG_VOUT_FS, TPS_FBR_2);
+    
+    
+    //Slew rate max
+    TPS55288_W_REG(SlaveAdr, REG_VOUT_SR, 3);
+    
+    
+    // set bot OCP_MASK
+    TPS55288_W_REG(SlaveAdr, REG_CDC, BuckBoost.CDC |0x40);
+}
+
+/**
+ * \fn void TPS55288_Write_VREF(uint8_t SlaveAdr, uint16_t vref)
+ * \brief fonction qui permet d'écrire dans la consigne de tension
+ * \return 0
+ */
+void TPS55288_Write_VREF(uint8_t SlaveAdr, uint16_t vref)
+{
+    uint8_t H,L;
+    
+    vref = vref & 0x3FF;
+            
+    H = vref >> 8;
+    L = vref & 0xFF;
+    
+    TPS55288_W_REG(SlaveAdr, REG_REF_LSB, L);
+    BuckBoost.VREF = TPS55288_R_REG(SlaveAdr, REG_REF_LSB);
+    
+    TPS55288_W_REG(SlaveAdr, REG_REF_MSB, H);
+    BuckBoost.VREF |= (uint16_t)(TPS55288_R_REG(SlaveAdr, REG_REF_MSB)<<8);
+}
+
+/*
+void TPS55288_Enable_Output()
+{
+    // Refresh internals registers
+    TPS55288_Read_ALL_REG(ADDR_TPS);
+    
+    // Si Output Enable ON
+    if((BuckBoost.MODE & 0x80) == 0)
+    {
+        // Pas d'output enable tant que une limite de courant est persistante
+        if(BuckBoost.OCP)
+        {
+            TPS55288_Init(ADDR_TPS);
+            TPS55288_SetVout(ADDR_TPS, ComputeVoutForThisMode(NORTEC,0.5)); 
+        }
+        // Pas de limite de courant atteint
+        else
+        {
+            TPS55288_Init(ADDR_TPS);
+        }
+    }
+    // Si Output Enable OFF
+    else
+    {
+        TPS55288_SetVout(ADDR_TPS, ComputeVoutForThisMode(NORTEC,1.0)); 
+    }
+}
+*/
+
+/**
+ * \fn void TPS55288_Read_STATUS(uint8_t SlaveAdr)
+ * \brief fonction qui permet de lire le registre de status
+ * le résultat est stocké dans la structure
+ * \return 0
+ */
+void TPS55288_Read_STATUS(uint8_t SlaveAdr)
+{
+    BuckBoost.STATUS = TPS55288_R_REG(SlaveAdr, REG_MODE);
+}
+
+/**
+ * \fn void TPS55288_Read_ALL_REG(uint8_t SlaveAdr)
+ * \brief fonction qui permet de lire l'ensemble des registres du composant
+ * le résultat est stocké dans la structure
+ * \return 0
+ */
+void TPS55288_Read_ALL_REG(uint8_t SlaveAdr)
+{
+    BuckBoost.VREF = TPS55288_R_REG(SlaveAdr, REG_REF_LSB);
+    BuckBoost.VREF |= (uint16_t)(TPS55288_R_REG(SlaveAdr, REG_REF_MSB)<<8);
+    BuckBoost.I_OUT_LIMIT = TPS55288_R_REG(SlaveAdr, REG_IOUT_LIMIT);
+    BuckBoost.VOUT_SR = TPS55288_R_REG(SlaveAdr, REG_VOUT_SR);
+    BuckBoost.VOUT_FS = TPS55288_R_REG(SlaveAdr, REG_VOUT_FS);
+    BuckBoost.CDC = TPS55288_R_REG(SlaveAdr, REG_CDC);
+    BuckBoost.MODE = TPS55288_R_REG(SlaveAdr, REG_MODE);
+    BuckBoost.STATUS = TPS55288_R_REG(SlaveAdr, REG_STATUS);
+    
+    BuckBoost.OCP = BuckBoost.STATUS>>6;
+    BuckBoost.OCP &= 0x01;
+    
+    BuckBoost.SCP = BuckBoost.STATUS>>7;
+    
+    BuckBoost.OE = BuckBoost.MODE>>7;
+}
+
+/**
+ * \fn void TPS55288_W_REG(uint8_t SlaveAdr, uint8_t RegAdr, uint8_t RegVal)
+ * \brief fonction qui permet d'écrire les registres du composant
+ * \return uint8_t
+ */
+void TPS55288_W_REG(uint8_t SlaveAdr, uint8_t RegAdr, uint8_t RegVal)
+{
+    I2C_Start();
+    
+    I2C_Write((SlaveAdr << 1));
+    I2C_Write(RegAdr);
+    
+    I2C_Write(RegVal);
+    I2C_Stop();
+}
+
+/**
+ * \fn uint8_t TPS55288_R_REG(uint8_t SlaveAdr, uint8_t RegAdr)
+ * \brief fonction qui permet de lire les registres du composant
+ * \return uint8_t
+ */
+uint8_t TPS55288_R_REG(uint8_t SlaveAdr, uint8_t RegAdr)
+{
+    uint8_t tmp = 0;
+    uint8_t val;
+    
+    I2C_Start();
+    
+    I2C_Write((SlaveAdr << 1));
+    I2C_Write(RegAdr);
+    
+    I2C_Restart();
+    tmp = (SlaveAdr << 1) | 0x01;
+    I2C_Write(tmp);
+    
+    val = I2C_Read();
+    I2C_NACK();
+    
+    I2C_Stop();
+    
+    return(val);
+}
---
+++ TPS55288.h
@@ -0,0 +1,58 @@
+
+#define ADDR_TPS 0x074
+
+
+
+#define REG_REF_LSB 0
+#define REG_REF_MSB 1
+#define REG_IOUT_LIMIT 2
+#define REG_VOUT_SR 3
+#define REG_VOUT_FS 4
+#define REG_CDC 5
+#define REG_MODE 6
+#define REG_STATUS 7
+
+#define TPS_OE_ON 0x80
+#define TPS_FBR_2 0x02
+
+
+#define TPS_IOUT_LIMIT_1_0A 0x14
+#define TPS_IOUT_LIMIT_1_2A 0x18
+#define TPS_IOUT_LIMIT_1_5A 0x1E
+#define TPS_IOUT_LIMIT_2A 0x28
+#define TPS_IOUT_LIMIT_MAX 0x7F
+#define TPS_IOUT_LIMIT_ENABLE 0x80
+
+#define TPS_STATE_OFF 0
+#define TPS_STATE_ON 1 
+
+
+
+struct TPS 
+{
+    uint16_t VREF;
+    uint8_t I_OUT_LIMIT;
+    uint8_t VOUT_SR;
+    uint8_t VOUT_FS;
+    uint8_t CDC;
+    uint8_t MODE;
+    uint8_t STATUS;
+    
+    uint8_t OE;
+    uint8_t OCP;
+    uint8_t SCP;
+};
+
+
+void TPS55288_Init(uint8_t SlaveAdr);
+
+
+void TPS55288_Read_ALL_REG(uint8_t SlaveAdr);
+//void TPS55288_Enable_Output();
+void TPS55288_SetVout(uint8_t SlaveAdr, float vout);
+void TPS55288_Write_VREF(uint8_t SlaveAdr, uint16_t vref);
+void TPS55288_Read_STATUS(uint8_t SlaveAdr);
+
+
+void TPS55288_W_REG(uint8_t SlaveAdr, uint8_t RegAdr, uint8_t RegVal);
+uint8_t TPS55288_R_REG(uint8_t SlaveAdr, uint8_t RegAdr);
